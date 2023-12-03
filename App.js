53e8f0eace6804c7910241700043deb20be9930e
SvetlanaBuuvol <divinesvetok@gmail.com>
2023-12-04 00:20:38 +0200

    some config for firebase

--- App.js
+++ App.js
@@ -1,21 +1,10 @@
 import "react-native-gesture-handler";
-// import { StatusBar } from "expo-status-bar";
 import React from "react";
-// import { NavigationContainer } from "@react-navigation/native";
-// import { createStackNavigator } from "@react-navigation/stack";
 import { useFonts } from "expo-font";
-// import RegistrationScreen from "./src/Screens/RegistrationScreen";
-// import LoginScreen from "./src/Screens/LoginScreen";
-// import MapScreen from "./src/Screens/MapScreen";
-// import ProfileScreen from "./src/Screens/ProfileScreen";
-// import PostsScreen from "./src/Screens/PostsScreen";
-// import CommentsScreen from "./src/Screens/CommentsScreen";
-// import CreatePostsScreen from "./src/Screens/CreatePostsScreen";
-// import headerOptions from "./src/assets/headerOptions/headerOptions";
-// import Home from "./src/Screens/Home";
+// import { PersistGate } from 'redux-persist/integration/react';
+// import { Provider } from 'react-redux';
+// import store from "./src/redux/store";
 import AppNavigation from "./src/components/AppNavigation";
-
-// const MainStack = createStackNavigator();
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -29,6 +18,13 @@ export default function App() {
   }
 
   return (
-    <AppNavigation/>
+    // <Provider store={store.store}>
+    //   <PersistGate
+    //     loading={<Text>Loading...</Text>}
+    //     persistor={store.persistor}
+    //   >
+        <AppNavigation />
+    //   </PersistGate>
+    // </Provider>
   );
 }
